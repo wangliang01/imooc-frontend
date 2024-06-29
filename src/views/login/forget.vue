93554ff25fa2bd08b84d9a7dfdc064247a9f311e
王亮 <1392830517@qq.com>
2024-06-29 15:42:10 +0800

    feat: register与forget页面样式

--- src/views/login/forget.vue
+++ src/views/login/forget.vue
@@ -1,13 +1,87 @@
 <template>
-  <div>
-    forget component
+  <div class="layui-container fly-marginTop">
+    <div class="fly-panel fly-panel-user" pad20>
+      <div class="layui-tab layui-tab-brief" lay-filter="user">
+        <ul class="layui-tab-title">
+          <li>
+            <router-link :to="{ name: 'login' }">登入</router-link>
+          </li>
+          <li class="layui-this">
+            找回密码
+            <!--重置密码-->
+          </li>
+        </ul>
+        <div
+          class="layui-form layui-tab-content"
+          id="LAY_ucm"
+          style="padding: 20px 0"
+        >
+          <div class="layui-tab-item layui-show">
+            <div class="layui-form layui-form-pane">
+              <form method="post">
+                <div class="layui-form-item">
+                  <label for="L_email" class="layui-form-label">用户名</label>
+                  <div class="layui-input-inline">
+                    <input
+                      type="text"
+                      name="username"
+                      v-model="username"
+                      placeholder="请输入用户名"
+                      autocomplete="off"
+                      class="layui-input"
+                    />
+                  </div>
+                  <div class="layui-form-mid">
+                    <!-- <span style="color: #c00;">{{errors[0]}}</span> -->
+                  </div>
+                </div>
+                <div class="layui-form-item">
+                  <div class="layui-row">
+                    <label for="L_vercode" class="layui-form-label"
+                      >验证码</label
+                    >
+                    <div class="layui-input-inline">
+                      <input
+                        type="text"
+                        name="code"
+                        v-model="code"
+                        placeholder="请输入验证码"
+                        autocomplete="off"
+                        class="layui-input"
+                      />
+                    </div>
+                    <div class>
+                      <span
+                        class="svg"
+                        style="color: #c00"
+                        @click="_getCode()"
+                        v-html="svg"
+                      ></span>
+                    </div>
+                  </div>
+                  <div class="layui-form-mid">
+                    <!-- <span style="color: #c00;">{{errors[0]}}</span> -->
+                  </div>
+                </div>
+                <div class="layui-form-item">
+                  <button
+                    type="button"
+                    class="layui-btn"
+                    alert="1"
+                    @click="submit()"
+                  >
+                    提交
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
